a2de6014ea739f5d89e759cad51a0ae62fcee114
Hakeemoloye <hakeemoloye@gmail.com>
2025-03-23 18:52:46 +0000

    resolução desafio1

---
+++ desafio1/src/main.rs
@@ -0,0 +1,27 @@
+// src/lib.rs
+
+pub unsafe fn multiply_array(ptr: *const i32, len: usize) -> i32 {
+    let mut product = 1;
+    for i in 0..len { // Começar a multiplicação do índice 0
+        product *= *ptr.offset(i as isize);
+    }
+    product
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_multiply_array() {
+        let arr = [2, 3, 4];
+        let product = unsafe { multiply_array(arr.as_ptr(), arr.len()) };
+        assert_eq!(product, 24);
+    }
+}
+
+fn main() {
+    println!("a multiplicação é de 24");
+}
+
+//o erro era provocado pois nao estava considersando o n0 para o calculo
